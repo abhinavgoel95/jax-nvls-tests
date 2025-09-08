3a329591abf3ba40e5c1d11bcad9c36fb57cc53f
Abhinav Goel <abhinavgoel95@gmail.com>
2025-09-08 12:39:39 -0700

    Create run_test.sh

---
+++ run_test.sh
@@ -0,0 +1,22 @@
+RUN_NAME=${1:-"testing_nvls"}
+
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.85
+export CUDA_DEVICE_MAX_CONNECTIONS=16
+
+export NCCL_NVLS_ENABLE=1
+export NCCL_ALGO=NVLS
+export NCCL_CTA_POLICY=1
+
+export TF_CPP_VMODULE=nccl_communicator=10,collective_thunk=10
+export TF_CPP_MIN_LOG_LEVEL=0
+export TF_CPP_MAX_LOG_LEVEL=10
+export NCCL_DEBUG=INFO
+export NCCL_DEBUG_SUBSYS=TUNING,ENV,INIT,REG
+export NCCL_NVLS_NCHANNELS=6
+
+export XLA_FLAGS="--xla_gpu_enable_nccl_user_buffers=true --xla_gpu_all_gather_combine_threshold_bytes=13421772800 --xla_gpu_reduce_scatter_combine_threshold_bytes=6710886400 --xla_gpu_experimental_enable_nccl_symmetric_buffers=true"
+
+mkdir -p /workspace
+NSYS_CMD="nsys profile --cuda-graph-trace=node -s none -o /path/to/profiles/${RUN_NAME}- --force-overwrite true --capture-range=cudaProfilerApi --capture-range-end=stop"
+
+${NSYS_CMD} python /path/to/simple_nvls_test.py
